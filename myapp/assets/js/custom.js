ebb9611c7436881ebc8bd495481d45bfe81a3b8e
Helen <liaforweb@gmail.com>
2019-08-15 19:56:25 +0300

    prav2

--- myapp/assets/js/custom.js
+++ myapp/assets/js/custom.js
@@ -267,7 +267,97 @@ $(".modal-open").click(function (e){
 
 });
 
+jQuery(window).resize(function($){
+    //slider bio
+    if(jQuery('.bio__slider').length) {
+        $('.bio__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: true,
+        dots: false,
+        // prevArrow: '<div class="arrow-prev"></div>',
+        // nextArrow: '<div class="arrow-next"></div>'
+        prevArrow: '<div class="arrow-prev"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>',
+        nextArrow: '<div class="arrow-next"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>'
+        });
+    }
+    if(jQuery('.events__slider').length) {
+        $('.events__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: true,
+        dots: false,
+        // prevArrow: '<div class="arrow-prev"></div>',
+        // nextArrow: '<div class="arrow-next"></div>'
+        prevArrow: '<div class="arrow-prev"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>',
+        nextArrow: '<div class="arrow-next"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>'
+        });
+    }    
+    if(jQuery('.diploms__slider').length) {
+        $('.diploms__slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        infinite: true,
+        arrows: true,
+        dots: true,
+        // prevArrow: '<div class="arrow-prev"></div>',
+        // nextArrow: '<div class="arrow-next"></div>'
+        prevArrow: '<div class="arrow-prev"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>',
+        nextArrow: '<div class="arrow-next"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z"/></svg></div>'
+        // , responsive: [
+        //     {
+        //       breakpoint: 1340,
+        //       settings: {
+        //         arrows: true,
+        //         dots: true
+        //       }
+        //     }
+        //   ]
+        });
+    }
+    if(jQuery('.consult__slider').length) {
+        $('.consult__slider-caption').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.consult__slider-for',
+            dots: true,
+            arrows: true,
+            centerMode: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false
+                }
+                }
+            ]
 
+        });
+        $('.consult__slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            fade: true,
+            asNavFor: '.consult__slider-caption',
+            prevArrow: '<div class="arrow-prev"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z" fill="#97DFEC"/></svg></div>',
+            nextArrow: '<div class="arrow-next"><svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.3536 4.35355C15.5488 4.15829 15.5488 3.84171 15.3536 3.64645L12.1716 0.464466C11.9763 0.269204 11.6597 0.269204 11.4645 0.464466C11.2692 0.659728 11.2692 0.976311 11.4645 1.17157L14.2929 4L11.4645 6.82843C11.2692 7.02369 11.2692 7.34027 11.4645 7.53553C11.6597 7.7308 11.9763 7.7308 12.1716 7.53553L15.3536 4.35355ZM0 4.5H15V3.5H0V4.5Z" fill="#97DFEC"/></svg></div>',
+            responsive: [
+                {
+                breakpoint: 1240,
+                settings: {
+                    arrows: true,
+                    dots: true
+                }
+                }
+            ]
+        });
+    }
+});
 
 
 document.addEventListener('DOMContentLoaded', function(){
